c8b67edb42d6afb5f33f1fa742c6bfd9fd11454e
nimisha173 <127300352+nimisha173@users.noreply.github.com>
2023-03-14 09:44:24 +0530

    Update employeegoldcoin.java

--- employeegoldcoin.java
+++ employeegoldcoin.java
@@ -1,3 +1,9 @@
+/* The employee who joined in an organization with 80days. starting from jan1 ,he recieved 5 gold coins,the employee joined 172 days and he got 3 gold coins.
+the employee 266 days -1 gold coin .*/
+
+
+
+
 package count;
 
 import java.util.stream.IntStream;
